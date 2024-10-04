592a5f31d192ec8ff478d07a4b6fddc781c65920
Anders Njøs Slinde <anders.slinde@gmail.com>
2024-10-04 23:27:58 +0200

    Add seed sql

---
+++ db/sql/seed.sql
@@ -0,0 +1,35 @@
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (1, "Lang svenske", 0,0, "https://kvann.no/wp-content/uploads/2019/11/20190316_Sortsbeskrivelse_poteter_utsendelse_vaaren2019.pdf");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (2, "Vestar", 0,0, "https://kvann.no/wp-content/uploads/2019/11/20190316_Sortsbeskrivelse_poteter_utsendelse_vaaren2019.pdf");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (3, "Knallfiffi", 0,0, "https://potet.no/produkter/knallfifi");
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (4, "Rød kvefjording", 0,0, "https://www.nibio.no/tema/mat/plantegenetiske-ressurser/nytteplanter-i-norge/jordbruksplanter/potet/rod-kvaefjording");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (5, "Tromøy", 0,0, "https://www.nibio.no/tema/mat/plantegenetiske-ressurser/nytteplanter-i-norge/jordbruksplanter/potet/tromoypotet");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (6, "Jøssing", 0,0, "https://www.nibio.no/tema/mat/plantegenetiske-ressurser/nytteplanter-i-norge/jordbruksplanter/potet/jossing");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (7, "Juno", 0,0, "https://potet.no/produkter/juno");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (8, "Kampion", 0,0, "https://kvann.no/produkt/kampion/");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (9, "Åspotet", 0,0, "https://www.nibio.no/tema/mat/plantegenetiske-ressurser/nytteplanter-i-norge/jordbruksplanter/potet/aspotet");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (10, "Mandel", 0,0, "https://www.bama.no/ravarer/poteter/mandelpotet/");
+
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (11, "Kerrs Pink", 0,0, "https://potet.no/produkter/kerrs-pink");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (12, "Blå Kongo", 0,0, "https://potet.no/produkter/bla-kongo");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (13, "Purple Peruvian", 0,0, "https://www.nibio.no/tema/mat/plantegenetiske-ressurser/nytteplanter-i-norge/jordbruksplanter/potet/purple-peruvian");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (14, "Kerrs Pink Blå", 0,0, "https://www.nibio.no/tema/mat/plantegenetiske-ressurser/nytteplanter-i-norge/jordbruksplanter/potet/kerrs-pink-med-blatt-skall");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (15, "King Edward", 0,0, "https://www.frukt.no/ravarer/poteter/king-edward/");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (16, "Svart Blå Skjåk", 0,0, "https://www.nibio.no/tema/mat/plantegenetiske-ressurser/nytteplanter-i-norge/jordbruksplanter/potet/svart-bla-fra-skjak");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (17, "Nansen ", 0,0, "https://potet.no/produkter/nansen");
+  
+INSERT INTO Potatoes (potatId, name, andersRating, linnRating, infoLink) VALUES (18, "Hassel ", 0,0, "https://potet.no/produkter/hassel");
+
